d674a1fd91a217904cbbe3a11786a9b0db560cb6
Abdalqader27 <abdalqader27.najjar@gmail.com>
2023-03-26 17:05:00 +0400

    feat: init project

---
+++ app/src/main/java/com/abdalqader27/zeta/ui/theme/Color.kt
@@ -0,0 +1,16 @@
+package com.abdalqader27.zeta.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val md_theme_light_primary = Color(0xFF00B8D4)
+val md_theme_light_secondary = Color(0xFFFF6D00)
+val md_theme_light_tertiary = Color(0xFF515E7D)
+val md_theme_light_background = Color(0xFFFAFDFD)
+val md_theme_light_surface = Color(0xFFFAFDFD)
+
+val md_theme_dark_primary = Color(0xFF4ED8E9)
+val md_theme_dark_secondary = Color(0xFFB1CBCF)
+val md_theme_dark_tertiary = Color(0xFFB9C6EA)
+val md_theme_dark_background = Color(0xFF191C1D)
+val md_theme_dark_surface = Color(0xFF191C1D)
+
---
+++ app/src/main/java/com/abdalqader27/zeta/ui/theme/Theme.kt
@@ -0,0 +1,67 @@
+package com.abdalqader27.zeta.ui.theme
+
+import android.app.Activity
+import android.os.Build
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.darkColorScheme
+import androidx.compose.material3.dynamicDarkColorScheme
+import androidx.compose.material3.dynamicLightColorScheme
+import androidx.compose.material3.lightColorScheme
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.SideEffect
+import androidx.compose.ui.graphics.toArgb
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalView
+import androidx.core.view.ViewCompat
+
+private val DarkColorScheme = darkColorScheme(
+    primary = md_theme_dark_primary,
+    secondary = md_theme_dark_secondary,
+    tertiary = md_theme_dark_tertiary,
+    background = md_theme_dark_background,
+    surface = md_theme_dark_surface,
+    )
+
+private val LightColorScheme = lightColorScheme(
+    primary = md_theme_light_primary,
+    secondary = md_theme_light_secondary,
+    tertiary = md_theme_light_tertiary,
+    background = md_theme_light_background,
+    surface = md_theme_light_surface,
+
+
+    )
+
+@Composable
+fun ZetaTheme(
+    darkTheme: Boolean = isSystemInDarkTheme(),
+    // Dynamic color is available on Android 12+
+    dynamicColor: Boolean = true,
+    content: @Composable () -> Unit
+) {
+    val colorScheme = when {
+        dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
+            val context = LocalContext.current
+            if (darkTheme)
+                dynamicDarkColorScheme(context)
+            else
+                dynamicLightColorScheme(context)
+        }
+        darkTheme -> DarkColorScheme
+        else -> LightColorScheme
+    }
+    val view = LocalView.current
+    if (!view.isInEditMode) {
+        SideEffect {
+            (view.context as Activity).window.statusBarColor = colorScheme.primary.toArgb()
+            ViewCompat.getWindowInsetsController(view)?.isAppearanceLightStatusBars = darkTheme
+        }
+    }
+
+    MaterialTheme(
+        colorScheme = colorScheme,
+        typography = Typography,
+        content = content
+    )
+}
